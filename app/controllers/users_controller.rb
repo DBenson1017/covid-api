9981bcfd9069a8499a7d80d57d43bf2a900f8741
vnapuri <vladnapuri@gmail.com>
2020-09-10 15:40:33 -0400

    thur 340  commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
         users = User.all
         render json: users 
     end 
+
+    def create
+        user = User.create!(user_params) 
+        render json: user
+    end
+
+    def show
+        user = User.find(params[:id]) 
+        render json: user   
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, 
+        :username, :email, :password, :state)
+    end
+
 end
